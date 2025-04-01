1d2f2a5df6fbfee3245cb6a8e9094c4d79276c76
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2025-04-01 15:07:24 +0600

    First commit

---
+++ src/main/java/com/aleksandr_kobelskiy/daily_calorie_tracker/DailyCalorieTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandr_kobelskiy.daily_calorie_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCalorieTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyCalorieTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aleksandr_kobelskiy/daily_calorie_tracker/DailyCalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandr_kobelskiy.daily_calorie_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCalorieTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
